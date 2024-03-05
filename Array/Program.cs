baac4fe86f7f306212dcfd4b0647e4deb72ef915
RaghavendraPai <RaghavendraPai@ZCHYDLP0005>
2024-03-05 12:33:03 +0530

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] a = {11,2,34,5,55,96,117,27,38,9};
+int temp;
+for (int i = 0; i < a.Length-1; i++)
+{
+    for (int j = 0; j < a.Length-i-1; j++)
+    {
+        if (a[j] >= a[j+1])
+        {
+            temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
+        }
+    }
+}
+foreach(int i in a)
+{
+    Console.Write(" "+i);
+}
